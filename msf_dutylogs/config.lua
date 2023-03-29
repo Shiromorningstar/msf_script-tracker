3892a7535720dffa2b8e0ddb434325a11a971750
Shiro MSF <91198430+Shiromorningstar@users.noreply.github.com>
2023-03-30 03:40:23 +0800

    Delete msf_dutylogs directory

--- msf_dutylogs/config.lua
+++
@@ -1,8 +0,0 @@
-Config                        		= {}
-
-
-Config.webhook_police    = ""
-Config.webhook_ems       = ""
-Config.webhook_mechanic  = ""
-Config.webhook_cityhall  = ""
-Config.webhook_cardealer = ""
